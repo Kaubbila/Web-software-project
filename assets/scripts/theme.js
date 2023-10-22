8bbf6f3933cf14ed1d8c0fc7b3df7ee2de50bbbf
Jaakko Kauppila <jaakko.kauppila1@gmail.com>
2023-10-22 17:34:31 +0300

    Javascript done

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -1,4 +1,25 @@
 function theme() {
-   var element = document.body;
+   const element = document.body;
    element.classList.toggle("dark-mode");
 }
+//Basic alert
+function submit() {
+   alert("Your submit was received!");
+}
+//Event listener.
+document.addEventListener("DOMContentLoaded", function() {
+   function submit() {
+       alert("Your submit was received!");
+   }
+   const submitButton = document.getElementById("submitButton");
+   submitButton.addEventListener("click", submit);
+});
+
+const prices = ["Socks", "Sandals", "Capybara", "Redemption", "World peace"];
+document.getElementById("array").innerHTML = prices;
+
+let text = "";
+for (let i = 0; i < prices.length; i++) {
+   text += prices[i] + "<br>";
+ }
+ document.getElementById("loop").innerHTML = text;
